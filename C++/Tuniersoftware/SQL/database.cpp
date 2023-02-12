02696d9699785f634342f8bbd0d92d208b939018
Max Schopf <max.schopf@mailfence.com>
2023-02-12 17:08:10 +0100

    Kein Richtiger Commit, nur rumspielen mit IDE Funktionen

---
+++ C++/Tuniersoftware/SQL/database.cpp
@@ -0,0 +1,11 @@
+#include "database.h"
+#include "stdio.h"
+
+Database::Database()
+{
+    printf("Object got created\n");
+}
+
+void Database::print(){
+    printf("Hey what\n");
+}
---
+++ C++/Tuniersoftware/SQL/database.h
@@ -0,0 +1,12 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+
+class Database
+{
+public:
+    Database();
+    void print();
+};
+
+#endif // DATABASE_H
